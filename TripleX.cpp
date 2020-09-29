85972514e590bdf7ef24ea4262f17324ba76aef5
kanderson425 <kyleanderson.dev@gmail.com>
2020-09-28 22:29:46 -0500

    First commit for TripleX game.

---
+++ TripleX.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "Enter the cooorrect code to continue...";
+    return 0;
+}
