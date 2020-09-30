6bdf74bd1a5a75ffc37d212ec819b967a9ed4549
kanderson425 <kyleanderson.dev@gmail.com>
2020-09-29 22:09:17 -0500

    Adds lesson code from the Functions & if-else statement lessons.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,21 +1,50 @@
 #include <iostream>
 
+void PrintIntroduction()
+{
+    // Print Welcome Messages;
+    std::cout << "You are a secret agent breaking into a secure server room...\n";
+    std::cout << "Enter the correct code to continue...\n";
+}
+
+void PlayGame() 
+{
+    PrintIntroduction();
+    
+    // Declare 3 number code
+    const int CodeA = 4;
+    const int CodeB = 3;
+    const int CodeC = 2;
+
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
+
+    // Print sum and product to the terminal
+    std::cout << std::endl;
+    std::cout << "There are 3 numbers in the code";
+    std::cout << "\nThe codes add-up to: ";
+    std::cout << "\nThe codes multiple to give: " << CodeProduct << std::endl;
+
+    // Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
+
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    // Check if the players guess is correct
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
+    {
+        std::cout << "Good job, you have broken the code!\n";
+    }
+    else {
+        std::cout << "No, that is not correct.\n";
+    }
+}
+
 int main()
 {
-    std::cout << "You are a secret agent breaking into a secure server room...";
-    std::cout << std::endl;
-    std::cout << "Enter the correct code to continue...";
-
-    int a = 4;
-    int b = 3;
-    int c = 2;
-
-    int sum = a + b + c;
-    int product = a * b * c;
-    
-    std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
-
+    PlayGame();
     return 0;
 }
