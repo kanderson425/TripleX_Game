e63ce40da416e718de302aabec380599ef82244b
kanderson425 <kyleanderson.dev@gmail.com>
2020-09-29 22:46:59 -0500

    Adds the completed TripleX game code.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
+#include <ctime>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
     // Print Welcome Messages;
-    std::cout << "You are a secret agent breaking into a secure server room...\n";
-    std::cout << "Enter the correct code to continue...\n";
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room...\nEnter the correct code to continue...\n";
 }
 
-void PlayGame() 
+bool PlayGame(int Difficulty) 
 {
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
     
     // Declare 3 number code
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+    const int CodeA = rand() % Difficulty + Difficulty;
+    const int CodeB = rand() % Difficulty + Difficulty;
+    const int CodeC = rand() % Difficulty + Difficulty;
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
@@ -23,7 +24,7 @@ void PlayGame()
     // Print sum and product to the terminal
     std::cout << std::endl;
     std::cout << "There are 3 numbers in the code";
-    std::cout << "\nThe codes add-up to: ";
+    std::cout << "\nThe codes add-up to: " << CodeSum;
     std::cout << "\nThe codes multiple to give: " << CodeProduct << std::endl;
 
     // Store player guess
@@ -36,15 +37,36 @@ void PlayGame()
     // Check if the players guess is correct
     if (GuessSum == CodeSum && GuessProduct == CodeProduct) 
     {
-        std::cout << "Good job, you have broken the code!\n";
+        std::cout << "/n*** Well done agent! you have extracted a file! Keep going! ***";
+        std::cout << "/n*** You entered the wrong code! Carefeul agent! Try again! ***";
+        return true;
     }
     else {
         std::cout << "No, that is not correct.\n";
+        std::cout << "Try again on this level.\n";
+        return false;
     }
 }
 
 int main()
 {
-    PlayGame();
+    srand(time(NULL)); // creates new sequence based on time of day
+
+    int LevelDifficulty = 1;
+    int const MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // our while loop is to play game until all 11 levels are played
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+        
+    }
+    std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
     return 0;
 }
